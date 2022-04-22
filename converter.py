a50afcb176b0ceaec941e2c1e3f544323c241f1b
Benjamin Eskola <ben@eskola.uk>
2022-04-22 14:54:10 +0100

    Only update used currencies

--- converter.py
+++ converter.py
@@ -7,8 +7,11 @@ import urllib.request
 from typing import Any, Dict, List, Optional
 
 CURRENCIES = json.load(open("currencies.json"))
-RATES: Dict[str, Dict[str, float]] = {}
 FAVOURITE_CURRENCIES = ["EUR", "GBP", "USD"]
+
+RATES: Dict[str, Dict[str, Any]] = {}
+if os.path.exists("rates.json"):
+    RATES = json.load(open("rates.json"))
 
 
 # would be nicer to use requests, but that may not be available.
@@ -18,7 +21,7 @@ def fetch(url: str) -> Any:
 
 
 def convert(val: float, from_cur: str, to_cur: str) -> Dict[str, Any]:
-    rate = RATES[from_cur][to_cur]
+    rate = RATES[from_cur]["rates"][to_cur]
     result = val * rate
     formatted_val = f"{val:.2f}".replace(".00", "")
     formatted_result = f"{result:.2f}".replace(".00", "")
@@ -27,14 +30,35 @@ def convert(val: float, from_cur: str, to_cur: str) -> Dict[str, Any]:
         "title": f"{formatted_result} {to_cur}",
         "arg": f"{formatted_result} {to_cur}",
         "text": {"largetype": f"{formatted_result} {to_cur}"},
-        "subtitle": f"{formatted_val} {CURRENCIES[from_cur]} in {CURRENCIES[to_cur]}",
+        "subtitle": f"{formatted_val} {CURRENCIES[from_cur]} in {CURRENCIES[to_cur]} @ 1 = {rate}",
     }
+
+
+def update_rates(curs: List[str]) -> None:
+    save_rates = False
+    now = time.time()
+
+    for cur in curs:
+        if cur not in RATES or RATES[cur]["timestamp"] < now - 86400:
+            print(f"updating {cur}", file=sys.stderr)
+            RATES[cur] = {
+                "timestamp": now,
+                "rates": fetch(f"https://api.exchangerate-api.com/v4/latest/{cur}")[
+                    "rates"
+                ],
+            }
+            save_rates = True
+
+    if save_rates:
+        print("saving rates", file=sys.stderr)
+        json.dump(RATES, open("rates.json", "w"))
 
 
 def main(
     val: float, from_cur: str, to_cur: Optional[str] = None
 ) -> List[Dict[str, Any]]:
     if not to_cur:
+        update_rates([from_cur.upper()] + FAVOURITE_CURRENCIES)
         return [
             convert(val, from_cur.upper(), to_cur.upper())
             for to_cur in FAVOURITE_CURRENCIES
@@ -42,31 +66,12 @@ def main(
         ]
 
     else:
+        update_rates([from_cur.upper(), to_cur.upper()])
         return [convert(val, from_cur.upper(), to_cur.upper())]
 
 
 if __name__ == "__main__":
     fetch_rates = True
-
-    if os.path.exists("rates.json"):
-        rate_data = json.load(open("rates.json"))
-        if rate_data["timestamp"] >= time.time() - 86400:
-            fetch_rates = False
-            RATES = rate_data["rates"]
-
-    if fetch_rates:
-        for currency in CURRENCIES.keys():
-            print(f"updating {currency}", file=sys.stderr)
-            response = fetch(
-                f"https://api.exchangerate-api.com/v4/latest/{currency.upper()}"
-            )
-            if "rates" in response:
-                RATES[currency.upper()] = response["rates"]
-
-        if RATES:
-            json.dump(
-                {"rates": RATES, "timestamp": time.time()}, open("rates.json", "w")
-            )
 
     params = sys.argv[1].split()
     val = float(params.pop(0))
